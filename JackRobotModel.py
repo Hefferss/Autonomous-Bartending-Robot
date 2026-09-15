68e161e1c651e51ffd0daa57bf2eb75aec9a3786
Hefferss <jack.heffernan78@gmail.com>
2026-09-15 17:25:56 +1000

    joint 1 finally done, had to eye it out

--- JackRobotModel.py
+++ JackRobotModel.py
@@ -5,10 +5,10 @@ from math import pi
 from ir_support import CylindricalDHRobotPlot
 import os
 from spatialgeometry import Mesh
-from spatialmath.base import trotx, trotz, troty
+from spatialmath.base import trotx, trotz, troty, transl
 
 #links for the staubli
-link1 = DHLink(d=0.375, a=0, alpha=pi/2, qlim=[-pi, pi])
+link1 = DHLink(d=0.3275, a=0, alpha=pi/2, qlim=[-pi, pi])
 link2 = DHLink(d=0, a=0.29, alpha=0, qlim=[-pi/2, pi/2])
 link3 = DHLink(d=0.02, a=0, alpha=-pi/2, qlim=[-pi/2, pi/2])
 link4 = DHLink(d=0.31, a=0, alpha=pi/2, qlim=[-pi, pi])
@@ -46,8 +46,7 @@ joint_1_mesh = Mesh(filename=joint_1_location, color="#7b1d1d")
 joint_check = robot.fkine_all(q) #checks all locations of the robot and lists. used to update position each time. maybe theres a better way?
 joint_1_mesh_place = joint_check[1].A
 joint_1_mesh.T = joint_1_mesh_place @ trotx(pi) @ troty(-pi/2)
-print("Cylinder joint 1 position:", joint_check[1].A[:3, 3])
-print("Mesh placed at:", joint_1_mesh.T[:3, 3])
+
 
 env.add(joint_1_mesh)
 env.step()
