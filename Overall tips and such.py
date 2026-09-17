bf7ebee2e54a2180f8a2e5ec02abb9a6c4db5437
Hefferss <jack.heffernan78@gmail.com>
2026-09-18 09:01:40 +1000

    check

---
+++ Overall tips and such.py
@@ -0,0 +1,12 @@
+Guide to git push:
+1. cd into wherever you want the project folder to live (e.g. cd "C:\Users\<your_username>\Documents")
+2. type in the terminal git clone https://github.com/Hefferss/Autonomous-Bartending-Robot.git
+3. that should open a clone of the git repository. Then do:
+4. git add .
+5. git commit -m "comment what you want to do or did here"
+6. git pull (merges with other versions of the files in case other people did work and it was pushed whilst you were working on your own part)
+7. git push (pushes your files onto the repository)
+
+
+
+you have to do cd .\Autonomous-Bartending-Robot
