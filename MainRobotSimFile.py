e625ecb8829724d708e73b4b35d20d9cd82a329d
Hefferss <jack.heffernan78@gmail.com>
2026-09-24 11:06:48 +1000

    moving and rotating stuff and adding new half lemon

--- MainRobotSimFile.py
+++ MainRobotSimFile.py
@@ -22,19 +22,19 @@ item_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Scene_pa
 #cognac scene load
 cognac_location = os.path.join(item_folder, "Cognac.stl")
 cognac_scene = Mesh(filename=cognac_location, color="#522B0F", scale=[0.001, 0.001, 0.001])
-cognac_scene.T = SE3(0.5, 0.5, 0.125) 
+cognac_scene.T = SE3(0.2, -0.2, 0.375) 
 env.add(cognac_scene)
 
 #cointreau.stl
 cointreau_location = os.path.join(item_folder, "Cointreau.stl")
 cointreau_scene = Mesh(filename=cointreau_location, color="#C47C07", scale=[0.02, 0.02, 0.02])
-cointreau_scene.T = SE3(0.5, 0.5, 0.01)
+cointreau_scene.T = SE3(0.3, -0.2, 0.25)
 env.add(cointreau_scene)
 
-#lemon.stl 
+#lemonSlice.stl 
 lemon_location = os.path.join(item_folder, "Lemon.stl")
 lemon_scene = Mesh(filename=lemon_location, color="#F7F7A1")
-lemon_scene.T = SE3(0.2, 0.2, 0.02)
+lemon_scene.T = SE3(-0.1, -0.1, 0.255) * troty(pi/2)
 env.add(lemon_scene)
 
 #BarTableRobot.stl
@@ -48,5 +48,11 @@ bar_table_storage_location = os.path.join(item_folder, "BarTableStorage.stl")
 bar_table_storage_scene = Mesh(filename=bar_table_storage_location, color="#8B4513", scale=[0.001, 0.001, 0.001])
 bar_table_storage_scene.T = SE3(-0.5, -0.5, 0)
 env.add(bar_table_storage_scene)
+
+#BetterLemon.stl
+Better_lemon_location = os.path.join(item_folder, "BetterLemon.stl")
+Better_lemon_scene = Mesh(filename=Better_lemon_location, color="#F7F7A1", scale=[0.001, 0.001, 0.001])
+Better_lemon_scene.T = SE3(-0.2, -0.1, 0.255) * troty(pi/2)
+env.add(Better_lemon_scene)
 
 input("Press Enter to continue...")
